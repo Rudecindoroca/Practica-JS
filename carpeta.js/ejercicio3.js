75ac2474ada6fc3e60c2eeac0ab0f0fab6ed90f1
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-05-14 17:04:42 -0300

    arranque con la practica de bucle

---
+++ carpeta.js/ejercicio3.js
@@ -0,0 +1,17 @@
+// practica bucle 
+
+// Precalento 
+
+
+// A
+
+for (let i = 0; i < 5; i++) {
+    console.log(`Practicando con bucle for`);
+} 
+
+
+// B
+
+for (let i = 0; i < 11; i++) {
+    console.log(`El valor de i es ${i}`);
+} 

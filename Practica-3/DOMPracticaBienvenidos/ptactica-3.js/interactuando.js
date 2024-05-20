eefbbd20850b490a7492a10dde31a341866a5ab3
Rudecindoroca <143553757+Rudecindoroca@users.noreply.github.com>
2024-05-20 14:53:12 -0300

    Termine la practica de DOM

--- Practica-3/DOMPracticaBienvenidos/ptactica-3.js/interactuando.js
+++ Practica-3/DOMPracticaBienvenidos/ptactica-3.js/interactuando.js
@@ -1,8 +1,11 @@
+
+
+// A
 let mensaje = "Bienvenidos a mi sitio.";
 
 alert(mensaje);  
 
-
+// B
 let deseaContinuar = confirm("¿Está seguro de querer avanzar?");
 let saludo = document.querySelector(".saludo");
 
@@ -13,25 +16,87 @@ if (deseaContinuar) {
 }
 
 
+// C
+
 let mensaje2 = "Por favor, ingrese su nombre";
 
 let nombre = prompt(mensaje2);
 
-
+// D
 document.querySelector("h1").innerText = `Bienvenido ${nombre}` ;
 
-
+// E
 let mensaje3 = "Cuantos años tenes?";
 
 
 let edad = prompt(mensaje3);
 
 
-if (edad >= 18) {
-    document.querySelector(".mayor-edad").innerText = "Sos mayor de edad";
+if (edad < 18) {
+    document.querySelector(".container-general").style.display = "none";
+    document.getElementById("accesoDenegado").style.display = "block";
+    
+} 
+
+// F
+let programacion = confirm("¿Te gusta la programación?");
+
+if (programacion === true) {
+
+  
+
+    document.querySelector(".container.background-img").style.backgroundImage = 'url("./img/programmer.jpeg")';
+    
 } else {
-    document
+    document.querySelector(".container.background-img").style.backgroundImage = 'url("./img/gatito.jpeg")';
 }
+
+
+
+
+// G
+
+
+let nuevaImagenURL = prompt("Por favor, ingresa la URL de la nueva imagen:");
+
+
+let avatarImg = document.querySelector("img.avatar");
+
+
+avatarImg.src = nuevaImagenURL;
+
+
+
+
+// H
+
+
+let pelicula = {};
+
+// I
+pelicula.nombre = prompt("Ingrese el nombre de su película favorita:");
+
+
+pelicula.director = prompt("Ingrese el nombre del director de la película:");
+
+
+pelicula.duracion = prompt("Ingrese la duración de la película (en minutos):");
+
+
+pelicula.actor = prompt("Ingrese el nombre del actor principal de la película:");
+
+
+let listaPelicula = document.querySelector("#pelicula ul");
+
+
+// J
+document.getElementById("nombre").innerText = "Nombre: " + pelicula.nombre;
+document.getElementById("director").innerText = "Director: " + pelicula.director;
+document.getElementById("duracion").innerText = "Duración: " + pelicula.duracion + " minutos";
+document.getElementById("actor").innerText = "Actor principal: " + pelicula.actor;
+
+let divPelicula = document.getElementById("pelicula");
+divPelicula.style.display = "block";
 
 
 
